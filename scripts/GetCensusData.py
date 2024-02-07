892c56d8b489c6b5c538a11761c89e56a4cf9d94
Audacity88 <dfgilles@uchicago.edu>
2024-02-06 19:00:40 -0600

    switching to wide format and intelligible column names

--- scripts/GetCensusData.py
+++ scripts/GetCensusData.py
@@ -10,28 +10,28 @@ with open('../census_key.txt', 'r') as f:
     key = f.read().strip()
 
 # get data from the census api and save to csv
-for census_type in ['acs1', 'acs5', 'acsse']:
+meanings = ['Total Population', 'Labor Force', 'Unemployed', 'Median Income']
+census_types = ['acs1', 'acs5', 'acsse']
+census_vars = {'acs1': 'B01001_001E,B23025_001E,B23025_005E,B19013_001E', \
+    'acs5': 'B01001_001E,B23025_001E,B23025_005E,B19013_001E', \
+    'acsse': 'K200104_001E,K202301_001E,K202301_005E,K201902_001E'}
+
+for census_type in census_types:
     counties = pd.DataFrame()
+    vars = census_vars[census_type]
     for year in range(2011, 2023):
-        if census_type == 'acsse':
-            census_vars = 'K200104_001E,K202301_001E,K202301_005E,K201902_001E'
-        else:
-            census_vars = 'B01001_001E,B23025_001E,B23025_005E,B19013_001E'
-        url = f"https://api.census.gov/data/{year}/acs/{census_type}?\
-            get={census_vars}&for=county:*&key={key}"
+        url = f"https://api.census.gov/data/{year}/acs/{census_type}?get={vars}&for=county:*&key={key}"
         r = requests.get(url)
         print(census_type, year, r.status_code)
         if r.status_code != 200:
             continue
         data = r.json()
         df = pd.DataFrame(data[1:], columns=data[0])
-        df['GeoID'] = df['state'] + df['county']
+        df['GEOID'] = df['state'] + df['county']
         df = df.drop(columns=['state', 'county'])
         df['Year'] = year
         counties = pd.concat([counties, df])
-    counties = counties.melt(id_vars=['GeoID', 'Year'], \
-                             value_vars=census_vars.split(','), \
-                                var_name='Variable', value_name='Value')\
-                                    .rename(columns={'Value': 'Estimate'})\
-                                        .sort_values(by=['GeoID', 'Year'])
+    counties.rename(columns=dict(zip(vars.split(','), meanings)), inplace=True)
+    counties = counties[['Year', 'GEOID'] + meanings]
+    display(counties)
     counties.to_csv(f"../data/census_data_{census_type}.csv", index=False)
